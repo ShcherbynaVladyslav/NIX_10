bbb82b0b86651fefa36130589eb673a89f6ffb60
Vlad <fakerok166@gmail.com>
2021-10-25 22:50:31 +0300

    module1

--- 1_module/src/main/java/ua/com/alevel/Main.java
+++ 1_module/src/main/java/ua/com/alevel/Main.java
@@ -11,25 +11,22 @@ public class Main {
 
         Triangle triangle = new Triangle();
         ArrayOfNumbers arrayOfNumbers = new ArrayOfNumbers();
-
+        Chess chess = new Chess();
 
         if (ss == 1) {
             System.out.println("Вы выбрали 1");
             System.out.println(s);
             triangle.run();
 
-
         } else if (ss == 2) {
             System.out.println("Вы выбрали 2");
             System.out.println(s);
             arrayOfNumbers.run(args);
 
-
-//        } else if (ss == 3) {
-//            System.out.println("Вы выбрали 3");
-//            System.out.println(s);
-//            task3.run();
-
+        } else if (ss == 3) {
+            System.out.println("Вы выбрали 3");
+            System.out.println(s);
+            chess.run();
 
         } else System.out.println("error");
     }
